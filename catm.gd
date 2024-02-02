e545360602a9a61922506dd4729990f977abb1a9
nabattis <ferreirafernando6205@gmail.com>
2024-02-02 22:48:46 +0000

    LETSSS GOOO

--- catm.gd
+++ catm.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @onready var _nav_agent = $NavigationAgent2D
 @onready var _sprite = $AnimatedSprite2D #Sprites
 
+var finished
 var speed = 75
 var move_to
 var screen_size # saves the screen size
@@ -18,43 +19,69 @@ enum PopupIds {
 
 
 func _ready():
+	finished = false
+	get_node("Timer").start()
 	screen_size = get_viewport_rect().size
-	move_to = Vector2(490,200)
+	move_to = position
 	_sprite.play("Idle" + color)
 	
 	#Adds items to the popup menu
 	_pm.add_item("Orange", PopupIds.colorOrange)
 	
 	
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta):
 	var dir
 	if Input.is_action_pressed("Right_Click") and selected:
+		finished = false
 		selected = false
 		move_to = get_global_mouse_position()
 	#dir = self.position.direction_to(move_to) 
-	dir = (_nav_agent.get_next_path_position() - global_position).normalized()
-	if self.position.distance_to(move_to) < 1:
-		get_node("Timer").stop()
-		_sprite.speed_scale = 1
-		_sprite.play("Idle" + color)
-		velocity = Vector2.ZERO
-	else: 
-		velocity = dir * speed
+	if not finished:
+		dir = (_nav_agent.get_next_path_position() - global_position).normalized()
+		translate(dir * delta * speed)
 		if dir.x > 0: _sprite.flip_h = false
 		else: _sprite.flip_h = true
 		_sprite.play("Running" + color)
 		_sprite.speed_scale = speed/50
-	move_and_slide()
-	print (_nav_agent.get_next_path_position())
+	else:
+		_sprite.speed_scale = 1
+		_sprite.play("Idle" + color)
+		
+	if self.position.distance_to(move_to) < 0.1:
+		finished = true
+	if not is_on_floor():
+		velocity.y = 350
+		move_and_slide()
+	position = position.clamp(Vector2.ZERO, screen_size)
+	#if self.position.distance_to(move_to) < 1:
+		#get_node("Timer").stop()
+		#_sprite.speed_scale = 1
+		#_sprite.play("Idle" + color)
+		#velocity = Vector2.ZERO
+	#else: 
+	#if finished:
+		#_sprite.speed_scale = 1
+		#_sprite.play("Idle" + color)
+	#if self.position.distance_to(move_to) < 1:
+		#dir = Vector2.ZERO
+		
+	#velocity = dir * speed
+	#if dir.x > 0: _sprite.flip_h = false
+	#else: _sprite.flip_h = true
+	#_sprite.play("Running" + color)
+	#_sprite.speed_scale = speed/50
+	#print (finished)
 
 func input_event(_viewport, event, _shape_idx):
 	var _last_mouse_position # variable that saves the last mouse position
-	
 	#Selects the cat in case the player clicked it
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if selected: selected = false
-			else: selected = true
+			if selected: 
+				selected = false
+			else: 
+				finished = false
+				selected = true
 
 		#Popup menu
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
@@ -63,10 +90,15 @@ func input_event(_viewport, event, _shape_idx):
 			
 
 func makepath() -> void:
-	_nav_agent.target_position = move_to
+	if not finished:
+		_nav_agent.target_position = move_to
 
 func _on_timer_timeout():
 	makepath()
+	
+func _on_navigation_agent_2d_target_reached():
+	#print(get_node("Timer").time_left)
+	finished = true
 
 #-------POPUP MENU FUNCTIONS---------
 
@@ -78,4 +110,6 @@ func _on_popup_menu_id_pressed(id):
 
 func _on_popup_menu_index_pressed(index):
 	pass # Replace with function body.
+
+
 
